393f8fc803e2318ca737b54a723b19d2e6aa7dd3
Shif_ <naters883@gmail.com>
2026-04-07 18:55:37 -0400

    started DH

--- arm.py
+++ arm.py
@@ -2,35 +2,23 @@ import sympy as sym
 import numpy as np
 import math
 
-class test:
-    def __init__(self, linkd, axis, setAngle = math.pi, angleLim = False):
-        
-        self.linkd = linkd
-
-        if axis in['x', 'y', 'z']:
-            this.axis = axis
-        else:
-            raise ValueError("not a valid rotational axis, must be x y or z")
-        
-        if type(anglelim) == 'list' and len(angleLim) == 2:
-            if anglelim[0] != anglelim[1]:
-                if anglelim[0] > anglelim[1]:
-                    this.anglelim[0] = anglelim[1]
-                    this.anglelim[1] = anglelim[0]
-
-                else:
-                    this.anglelim = anglelim
-
-            else:
-                raise ValueError("invalid angle range")
-        else:
-            raise ValueError("Angle limit invalid, leave empty for no limit or enter 2 different angles")
-        
-        if this.angleLim[0] <= setAngle <= this.angleLim[1]:
-                this.angle = angle
-        else:
-            raise ValueError("Start angle outside of limits")
-
-
 class link:
-    
+    def __init__(self, omega, d, a, alpha, Jlim, joint='r'):
+        this.omega = omega
+        this.d = d
+        this.a = a
+        this.alpha = alpha
+
+        if Jlim[0] != Jlim[1] and len(Jlim) == 2:
+            this.Jlim = JLim.sort()
+
+        this.joint = joint
+
+    def getDH_revolute(self):
+        a
+
+    def getDH_prismatic(self):
+        a
+
+    def getDH(self):
+        return self.getDH_revolute() if self.joint == 'r' else self.getDH_prismatic
