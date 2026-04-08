b9df0c9ef15d8ef05c300555714457449fef3d5e
Shif_ <naters883@gmail.com>
2026-04-07 23:51:56 -0400

    Forward Kinematics work

--- arm.py
+++ arm.py
@@ -1,24 +1,70 @@
-import sympy as sym
+import sympy
 import numpy as np
-import math
 
-class link:
-    def __init__(self, omega, d, a, alpha, Jlim, joint='r'):
-        this.omega = omega
-        this.d = d
-        this.a = a
-        this.alpha = alpha
+class links:
+    def __init__(self, dORtheta, a, alpha, Jlim, joint='r'):
+        self.a = a
+        self.alpha = alpha
+
+        if joint == 'r':
+            self.theta = sympy.Symbol(f'theta_{id(self)}')
+            self.d = dORtheta
+        
+        elif joint == 'p':
+            self.d = sympy.Symbol(f'd_{id(self)}')
+            self.theta = dORtheta
 
         if Jlim[0] != Jlim[1] and len(Jlim) == 2:
-            this.Jlim = JLim.sort()
+            self.Jlim = sorted(Jlim)
 
-        this.joint = joint
-
-    def getDH_revolute(self):
-        a
-
-    def getDH_prismatic(self):
-        a
+        self.joint = joint
+        
+        self.DH = self.getDH()
 
     def getDH(self):
-        return self.getDH_revolute() if self.joint == 'r' else self.getDH_prismatic
+        ctheta = sympy.cos(self.theta)
+        stheta = sympy.sin(self.theta)
+        calpha = sympy.cos(self.alpha)
+        salpha = sympy.sin(self.alpha)
+
+        a = self.a
+        d = self.d
+
+        DH = sympy.Matrix([
+            [ctheta, (-stheta * calpha), (stheta * salpha), (a * ctheta)],
+            [stheta, (ctheta * calpha), (-ctheta * salpha), (a * stheta)],
+            [0, salpha, calpha, d],
+            [0, 0, 0, 1]
+        ])
+        return DH
+    
+def getSetDH(arm, n):
+    if n <= len(arm):
+        DH = arm[0].DH
+        for i in range(1, n):
+            DH = DH * arm[i].DH
+        return DH
+    else:
+        raise ValueError("not enough links, or invalid link index")
+
+
+def getAllPos(arm, angles):
+    allPos = []
+
+    for i in range(0, len(arm)):
+        DH = getSetDH(arm, (i + 1))
+        sub = []
+        for j in range(0, i + 1):
+            if arm[j].joint == 'r':
+                sub.append([arm[j].theta,angles[j]])
+            else:
+                sub.append([arm[j].d,angles[j]])
+        
+        pos = DH.subs(sub)
+        x = float(pos[0, 3])
+        y = float(pos[1, 3])
+        z = float(pos[2, 3])
+
+        allPos.append([x, y, z])
+    
+    return allPos
--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-from arm import link
+import math
+from arm import *
 
+
+link1 = links(0, 3, math.radians(90), [-math.pi, math.pi])
+link2 = links(0, 2, 0, [-math.pi, math.pi])
+link3 = links(0, 2, -math.radians(90), [-math.pi, math.pi])
+link4 = links(0, 1, 0, [-math.pi, math.pi])
+
+arm = [link1, link2, link3, link4]
+
+angles = [math.radians(30), math.radians(45), -math.radians(20), math.radians(60)]
+pos1 = getAllPos(arm, angles)
+
+for row in pos1:
+    print(row)
