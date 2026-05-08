3c2c72e7e270911d8af0f88495cd7d6b03f76fb3
Shif_ <naters883@gmail.com>
2026-05-08 19:56:59 -0400

    leaving laptop

--- tests/testPath.py
+++ tests/testPath.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 # Adds the parent directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -9,7 +8,6 @@ import arm
 import path
 import plot
 
-import numpy as np
 import math
 
 Arm = []
@@ -19,7 +17,18 @@ Arm.append(arm.link(0, 0, 3, 0))
 Arm.append(arm.link(3, 0, 0, math.pi/2))
 Arm.append(arm.link(1, 0, 0, -math.pi/2))
 Arm.append(arm.link(3, 0, 0, 0))
-
+Arm.append(arm.link(2, 0, 0, math.pi/2))
+Arm.append(arm.link(0, 0, 3, 0))
+Arm.append(arm.link(0, 0, 3, 0))
+Arm.append(arm.link(3, 0, 0, math.pi/2))
+Arm.append(arm.link(1, 0, 0, -math.pi/2))
+Arm.append(arm.link(3, 0, 0, 0))
+Arm.append(arm.link(2, 0, 0, math.pi/2))
+Arm.append(arm.link(0, 0, 3, 0))
+Arm.append(arm.link(0, 0, 3, 0))
+Arm.append(arm.link(3, 0, 0, math.pi/2))
+Arm.append(arm.link(1, 0, 0, -math.pi/2))
+Arm.append(arm.link(3, 0, 0, 0))
 
 anglesPI = [math.pi, math.pi, math.pi, math.pi, math.pi, math.pi]
 
@@ -40,11 +49,8 @@ path1.addP(path.point(b, b, -1, math.pi, 0, math.pi))
 path1.addP(path.point(b, b+1, -1, math.pi, 0, math.pi))
 path1.addP(path.point(b, b, -1, math.pi, 0, math.pi))
 
-path1.addP(path.point(b+1, b, -1, math.pi, 0, math.pi))
-path1.addP(path.point(b, b, -1, math.pi, 0, math.pi))
-
-path1.addP(path.point(b, b, 0, math.pi, 0, math.pi))
-path1.addP(path.point(b, b, -1, math.pi, 0, math.pi))
+path1.addP(path.point(b+1, b, -1, math.pi, math.pi, -math.pi))
+path1.addP(path.point(b, b, -1, math.pi, 10, math.pi))
 
 
 
@@ -63,8 +69,8 @@ path1.addP(path.point(0, a, -1, math.pi, -math.pi/2, math.pi))
 path1.addP(path.point(a, a, 1, math.pi, 0, math.pi))
 path1.addP(path.point(a, a, 2-heightDiff, math.pi, 0, math.pi))
 
-PointsPerSecond = 200       # the number of points along a line per second of movement (based on the speed, determines animation framerate)
-UnitsPerSecond = 5         # the speed at which the endeffector will move along the path in units per second
+PointsPerSecond = 60       # the number of points along a line per second of movement (based on the speed, determines animation framerate)
+UnitsPerSecond = 1         # the speed at which the endeffector will move along the path in units per second
 
 ps1 = path.pointSet(path1, Arm, "p2p",PointsPerSecond,UnitsPerSecond)
 pathPoints = ps1.updateCheck()
@@ -73,4 +79,4 @@ ps1.generatePoints()
 print("Calculating....")
 Parm, Pangles, Ppoints, Pkey = ps1.getPathData()
 
-plot.dispPath(Arm=Parm,Angles=Pangles,Points=Ppoints,PPs=PointsPerSecond,keyPos=Pkey)
+plot.dispPath(Arm=Parm,Angles=Pangles,Points=Ppoints,PPs=PointsPerSecond,keyPos=Pkey)   
