699a465d1c942984b19fbf96eae0a9d70f670f1d
Shif_ <naters883@gmail.com>
2026-05-11 16:28:55 -0400

    worked on physics

--- physics.py
+++ physics.py
@@ -9,6 +9,10 @@ import matplotlib.animation as animation
 
 from arm import getAllTransform, getAllPos
 
+# gravity
+g = [0, 0, -9.81] # m/s^2, down in the z
+
+
 def get_kinematic(Arm,valueSet,PPs, v_i=None,w_i=None,v_f=None,w_f=None,loop=True):
     transforms = getAllTransform(Arm,valueSet)
     interval = 1 / PPs
@@ -133,6 +137,29 @@ class link_physics:
         
         else:
             raise ValueError("currently can't do anything but a slender bar")
+        
+    def get_link_kinetic_energy(self,angular,velocity):
+        angular = np.array(angular)
+        velocity = np.array(velocity)
+
+        T_linear = (1 / 2) * (self.mass * velocity ** 2)
+        T_rotational = (1 / 2) * (self.I_Cm * angular ** 2)
+
+        T = T_linear + T_rotational
+        return T
+    
+    def get_gravitational_potential_energy(self, height, gravity=np.linalg.norm(g)):
+        PE = self.mass * gravity * height
+        return PE
+    
+    def link_energy(self, height, angular, velocity):
+        T = self.get_link_kinetic_energy(angular,velocity)
+        PE = self.get_gravitational_potential_energy(height)
+
+        energy = T + PE
+
+        return energy
+
 
 def get_arm_link_properties(Arm,values=None,type="normal"):
     points = np.array(getAllPos(arm=Arm,values=values))
@@ -161,24 +188,24 @@ def get_arm_link_properties(Arm,values=None,type="normal"):
     else:
         raise ValueError("Unsupported property type")
 
+def get_all_arm_properties(Arm, valueSet,type):
+    properties = []
+    for values in valueSet:
+        item = get_arm_link_properties(Arm,values,type)
+        properties.append(item)
+    
+    return properties
+
 def get_simple_physics_arm(Arm,kg_m=None):
     lengths = get_arm_link_properties(Arm=Arm,type="mag")
     directions = get_arm_link_properties(Arm=Arm,type="unit")
 
-    joint_num = len(Arm)
-
     physics_arm = []
     for length, direction in zip(lengths, directions):
         link = link_physics(length=length,direction=direction,kg_m=kg_m,)
-        
-
-
-
-        
-
-
-
-
+        physics_arm.append(link)
+    return physics_arm
+           
 def slenderbar_Iyy(m,L):
     Iyy = (1/12) * m * (L ** 2)
     return Iyy
@@ -186,3 +213,73 @@ def slenderbar_Iyy(m,L):
 def slenderbar_Iy1(m,L):
     Iy1 = (1/3) * m * (L ** 2)
     return Iy1
+
+def get_all_CoM(Arm, scientific_Arm, valueSet):
+
+    frame_num = len(valueSet)
+    directions = get_all_arm_properties(Arm,valueSet,"unit")
+
+    arm_positions = []
+    for values in valueSet:
+        pos = getAllPos(Arm,values)
+        arm_positions.append(pos)
+
+    CM_magnitude = []
+    for physics_link in scientific_Arm:
+        CM = physics_link.Cm_joint
+        CM_magnitude.append(CM)
+    
+    CM_all = np.tile(CM_magnitude,(frame_num, 1))
+
+    COM_pos = [] # frame, joint, coordinate of CoM for that joint
+    for frame_CM, frame_pos, frame_directons in zip(CM_all, arm_positions, directions):
+        frame_COM_pos = []
+        for joint_CM, joint_pos, joint_direction in zip(frame_CM, frame_pos, frame_directons):
+            vec = np.array(joint_direction) * joint_CM
+            pos = joint_pos + vec
+            frame_COM_pos.append(pos)
+        COM_pos.append(frame_COM_pos)
+
+def get_arm_energy(Arm, valueSet,scientific_Arm, angular=None, velocity=None): # frame, joint, value (valueset)
+    W = get_kinematic(Arm,valueSet=valueSet)
+    motion_components = get_motion_components(W)
+
+    if velocity is None:
+        velocity = motion_components[0]
+
+    if angular is None:
+        angular = motion_components[1]
+
+    arm_positions = []
+    for values in valueSet:
+        pos = getAllPos(Arm,values)
+        arm_positions.append(pos)
+
+    CoM_pos = np.array(get_all_CoM(Arm, scientific_Arm, valueSet))
+
+    heights = CoM_pos[:, :, 2] #only z
+
+    all_energy = []
+    for frame_velocity, frame_angular, frame_height in zip(velocity, angular, heights):
+        frame_energy = []
+        for joint_velocity, joint_angular, joint_height, scientificlink in zip(frame_velocity, frame_angular, frame_height, scientific_Arm):
+            link_energy = scientificlink.link_energy(joint_height, joint_angular, joint_velocity)
+            frame_energy.append(link_energy)
+        all_energy.append(frame_energy)
+
+    return all_energy
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
